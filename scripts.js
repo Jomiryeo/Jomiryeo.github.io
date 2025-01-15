7bc7080737439c5abd082041c06fcec9c28b4ac6
Jomiryeo <cartersmayer@gmail.com>
2025-01-15 10:59:41 +0900

    ffs

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Function to toggle the menu visibility on mobile screens
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
